1d900938049dca68a3b07db7b45f7964d984c0ce
Andrew Dang <{ID}+{username}@users.noreply.github.com>
2022-06-17 10:19:31 -0400

    added tests for services

--- tests/test_request_service.py
+++ tests/test_request_service.py
@@ -1,7 +1,8 @@
 import json
 import pytest
+from database.request_dao import insert_user_request
 
-from service.request_service import get_all_pending_requests, update_request_status
+from service.request_service import cancel_pending_request, get_all_pending_requests, update_request_status
 
 @pytest.mark.parametrize("user_id, expected", [
     (1, True), (99, True) 
@@ -26,5 +27,19 @@ def test_pending_request_returns_json(req_id, status, expected):
     try:
         val = json.loads(json_pending)
         assert val.get('attempt_update') == expected
+    except ValueError as err:
+        assert True
+
+@pytest.mark.parametrize("is_new_req, expected", [
+    (True, True), (False, True)
+])
+def test_cancel_request_returns_json(is_new_req, expected):
+    req_id = 0
+    if is_new_req:
+        req_id = insert_user_request(2, 500, "Default Description")
+    json_pending = cancel_pending_request(req_id)
+    try:
+        val = json.loads(json_pending)
+        assert val.get('attempt_cancel') == expected
     except ValueError as err:
         assert True
