ebd858962afb8159115791c8badddca804068ac5
Andrew Dang <{ID}+{username}@users.noreply.github.com>
2022-06-19 17:35:32 -0400

    uncommented the update request_dao function

--- database/request_dao.py
+++ database/request_dao.py
@@ -91,7 +91,7 @@ def update_req_by_req_id(req_id, var_Name, value):
 
     try:
         cur.execute(qry)
-        #conn.commit()
+        conn.commit()
         #have to return false if no row is updated 
         if cur.rowcount == 0:
             return False
